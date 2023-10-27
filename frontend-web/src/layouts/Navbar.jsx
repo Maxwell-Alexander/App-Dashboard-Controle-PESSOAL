6f09c8aa63dd20008dc6864efaf88042698b5a5c
Maxwell Alexander <maxwell.alexander@volus.com>
2023-10-27 11:13:47 -0300

    Adição do icone de expansão e retração do Sidebar

--- frontend-web/src/layouts/Navbar.jsx
+++ frontend-web/src/layouts/Navbar.jsx
@@ -1,6 +1,3 @@
-//import React, { useState } from 'react';
-
-
 import styles from '../styles/Navbar.module.css';
 import { CiMenuBurger } from "react-icons/ci";
 
@@ -8,12 +5,35 @@ import { CiMenuBurger } from "react-icons/ci";
 
 function Navbar() {
 
+    let openSidebar = false;
+
+    function Expandir() {
+
+        let sidebar = document.getElementById('sidebar');
+        let root = document.getElementById('root');
+
+
+        if (openSidebar == false) {
+            openSidebar = true;
+            root.style.gridTemplateColumns = '220px';
+            sidebar.style.width = '220px';
+
+
+        } else if (openSidebar) {
+            openSidebar = false;
+            root.style.gridTemplateColumns = '70px'
+            sidebar.style.width = '70px';
+
+        }
+
+    }
+
 
 
     return (
         <div className={styles.navbar}>
 
-            <div id={styles.iconExpandir} >
+            <div id={styles.iconExpandir} onClick={Expandir}>
                 <CiMenuBurger />
             </div>
 
